f9ca8eda3b0ef107c37c92919092a545ea4398da
devdot4 <15527358+devdot4@users.noreply.github.com>
2021-01-05 15:38:25 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, I am devdot4!'));
+app.get('/', (req, res) => res.send('Hello World, I am devdot4!. I just built my new app!!!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
